b75127b7499dd320d3aec6d1c499408db9607076
Alice <nj19721@bristol.ac.uk>
2020-11-13 16:32:49 +0000

    Single thread part 1

--- Concurrent/gol/distributor.go
+++ Concurrent/gol/distributor.go
@@ -1,5 +1,7 @@
 package gol
 
+import "uk.ac.bris.cs/gameoflife/util"
+
 type distributorChannels struct {
 	events    chan<- Event
 	ioCommand chan<- ioCommand
@@ -10,11 +12,26 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight, p.ImageWidth)
 	// TODO: For all initially alive cells send a CellFlipped Event.
+	for i, elem := range world {
+		for j, cell := range elem {
+			if cell == 255 {
+				d := util.Cell{X: i, Y: j}
+				cellFlip := CellFlipped{CompletedTurns: 0, Cell: d}
+				c.events <- cellFlip
+			}
+		}
+	}
 
 	turn := 0
 
 	// TODO: Execute all turns of the Game of Life.
+	for x := 0; x < p.Turns; x++ {
+		world = calculateNextState(p, world, c, x)
+		c.events <- TurnComplete{CompletedTurns: x}
+	}
+
 	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
 	//		 See event.go for a list of all events.
 
@@ -25,4 +42,88 @@ func distributor(p Params, c distributorChannels) {
 	c.events <- StateChange{turn, Quitting}
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func calculateAliveNeighbours(h int, w int, world [][]byte, x int, y int) int {
+	ans := 0
+	up := (x + 1) % h
+	down := (x - 1) % h
+	if down < 0 {
+		down = down + h
+	}
+	right := (y + 1) % w
+	left := (y - 1) % w
+	if left < 0 {
+		left = left + w
+	}
+
+	if world[left][x] == 255 {
+		ans++
+	}
+	if world[right][x] == 255 {
+		ans++
+	}
+	if world[y][down] == 255 {
+		ans++
+	}
+	if world[y][up] == 255 {
+		ans++
+	}
+	if world[right][down] == 255 {
+		ans++
+	}
+	if world[right][up] == 255 {
+		ans++
+	}
+	if world[left][up] == 255 {
+		ans++
+	}
+	if world[left][down] == 255 {
+		ans++
+	}
+	return ans
+}
+
+func calculateNextState(p Params, world [][]byte, c distributorChannels, x int) [][]byte {
+	h := p.ImageHeight
+	w := p.ImageWidth
+	nworld := make([][]byte, h, w)
+	for y, a := range world {
+		nA := make([]byte, w)
+		for x, b := range a {
+			nB := byte(0)
+			ln := calculateAliveNeighbours(h, w, world, x, y)
+			if b == 0 {
+				if ln == 3 {
+					nB = 255
+					cell := util.Cell{X: x, Y: y}
+					c.events <- CellFlipped{CompletedTurns: x, Cell: cell}
+				} else {
+					nB = 0
+				}
+			} else {
+				if ln < 2 || ln > 3 {
+					nB = 0
+				} else {
+					nB = 255
+				}
+			}
+			nA[x] = nB
+		}
+		nworld[y] = nA
+	}
+	return nworld
+}
+
+func calculateAliveCells(p golParams, world [][]byte) []cell {
+	alive := []cell{}
+	for y, a := range world {
+		for x, b := range a {
+			if b == 255 {
+				coord := cell{x, y}
+				alive = append(alive, coord)
+			}
+		}
+	}
+	return alive
 }
